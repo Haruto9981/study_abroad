e973e649bb30fad3d586bb5ad5ec6542da9c7f46
Haruto9981 <aketaharuto46@icloud.com>
2023-08-07 14:14:51 +0900

    implementation of the authorization

--- resources/views/diaries/create.blade.php
+++ resources/views/diaries/create.blade.php
@@ -4,24 +4,30 @@
         <meta charset="utf-8">
         <title>Diary</title>
     </head>
-    <body>
-        <h1>Create Diary</h1>
-        <form action="/diaries" method="POST">
-            @csrf
-            <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="diary[title]" placeholder="Title"/>
-                <p class="title__error" style="color:red">{{ $errors->first('diary.title') }}</p>
+    <x-app-layout>
+        <x-slot name="header">
+           <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Create Diary') }}
+           </h2>
+        </x-slot>
+        <body>
+            <form action="/diaries" method="POST">
+                @csrf
+                <div class="title">
+                    <h2>Title</h2>
+                    <input type="text" name="diary[title]" placeholder="Title"/>
+                    <p class="title__error" style="color:red">{{ $errors->first('diary.title') }}</p>
+                </div>
+                <div class="content">
+                    <h2>Content</h2>
+                    <textarea name="diary[content]" placeholder="What's your experience?"></textarea>
+                     <p class="content__error" style="color:red">{{ $errors->first('diary.content') }}</p>
+                </div>
+                <input type="submit" value="Create"/>
+            </form>
+            <div class="footer">
+                <a href="/">戻る</a>
             </div>
-            <div class="content">
-                <h2>Content</h2>
-                <textarea name="diary[content]" placeholder="What's your experience?"></textarea>
-                 <p class="content__error" style="color:red">{{ $errors->first('diary.content') }}</p>
-            </div>
-            <input type="submit" value="Create"/>
-        </form>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+        </body>
+    </x-app-layout>
 </html>
--- resources/views/diaries/show.blade.php
+++ resources/views/diaries/show.blade.php
@@ -7,21 +7,28 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">
-            {{ $diary->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <p>{{ $diary->content }}</p> 
+    <x-app-layout>
+        <x-slot name="header">
+           <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Diary') }}
+           </h2>
+        </x-slot>
+        <body>
+            <h1 class="title">
+                {{ $diary->title }}
+            </h1>
+            <div class="content">
+                <div class="content__post">
+                    <p>{{ $diary->content }}</p> 
+                </div>
+                <div class="photo">
+                    <p class='photo'>{{$diary->photo}}</p>
+                </div>
             </div>
-            <div class="photo">
-                <p class='photo'>{{$diary->photo}}</p>
+            <div class="edit"><a href="/diaries/{{ $diary->id }}/edit">Edit</a></div>
+            <div class="footer">
+                <a href="/">戻る</a>
             </div>
-        </div>
-        <div class="edit"><a href="/diaries/{{ $diary->id }}/edit">Edit</a></div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+        </body>
+     </x-app-layout>
 </html>
