356072027d84e4b9ad3b72f8fb2100d83c36d164
Haruto9981 <aketaharuto46@icloud.com>
2023-09-11 21:52:36 +0900

    fix the layout of the login, register and update profile

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,48 +2,65 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
         
-        <div>
+        <h2 class="text-xl">Fill in your Profile</h2>
+        
+        <div class="flex justify-center">
+            <label for="profile-image">
+                <img alt="Profile image" src="storage/profiles/default.png" class="w-20 h-20 rounded-full  object-cover object-center">
+                <input id="profile-image" name="profile_image" type="file" class="form-control @error('profile-image') is-invalid @enderror" style="display:none;" value="" accept="image/png, image/jpeg">
+                <x-input-error :messages="$errors->get('profile_image')" class="mt-2" />
+            </label>
+        </div>
+        <div class="flex justify-center mt-2">
+            <h2 class="text-sm">Choose your profile image!</h2>
+        </div>
+        
+       
+        
+        <div class="mt-2 mb-4">
             <label for="name">{{ __('Name') }}</label>
-            <input id="name" class="block mt-1 w-full" type="text" name="name" value="{{old('name')}}" required autofocus autocomplete="name" />
+            <input id="name" class="block mt-1 w-full rounded-lg" type="text" name="name" value="{{old('name')}}" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         
-        <div>
+        <div class="my-4">
             <label for="gender">{{ __('Gender') }}</label>
             <table>
                 <tr>
-                    <th><input id="gender-m" class="block mt-1" type="radio" name="gender" value="male" required autofocus /></th>
+                    <th class="px-1"><input id="gender-m" class="block mt-1" type="radio" name="gender" value="male" required autofocus /></th>
                     <td><label for="gender-m">{{__('Male')}}</label></td>
-                    <th><input id="gender-f" class="block mt-1" type="radio" name="gender" value="female" required autofocus /></th>
+                    <th class="px-1"><input id="gender-f" class="block mt-1" type="radio" name="gender" value="female" required autofocus /></th>
                     <td> <label for="gender-f">{{__('Female')}}</label></td>
-                    <th><input id="gender-o" class="block mt-1" type="radio" name="gender" value="other" required autofocus /></th>
+                    <th class="px-1"><input id="gender-o" class="block mt-1" type="radio" name="gender" value="other" required autofocus /></th>
                     <td> <label for="gender-o">{{__('Other')}}</label></td>
                 </tr>
             </table>
             <x-input-error :messages="$errors->get('sex')" class="mt-2" />
         </div>
 
-        <div>
+        <div class="my-4">
             <label for="email">{{ __('Email') }}</label>
-            <input id="email" class="block mt-1 w-full" type="email" name="email" value="{{old('email')}}" required autofocus autocomplete="username" />
+            <input id="email" class="block mt-1 w-full rounded-lg" type="email" name="email" value="{{old('email')}}" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
        
-        <div>
+        <div class="my-4">
             <label for="password">{{ __('Password') }}</label>
-            <input id="password" class="block mt-1 w-full" type="password" name="password" value="{{old('password')}}" required autofocus autocomplete="new-password" />
+            <input id="password" class="block mt-1 w-full rounded-lg" type="password" name="password" value="{{old('password')}}" required autofocus autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div> 
         
-        <div>
+        <div class="my-4">
             <label for="password_confirmation">{{ __('Confirm Password') }}</label>
-            <input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" value="{{old('password_confirmation')}}" required autofocus autocomplete="new-password" />
+            <input id="password_confirmation" class="block mt-1 w-full  rounded-lg" type="password" name="password_confirmation" value="{{old('password_confirmation')}}" required autofocus autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div> 
         
-        <div>
-            <label for="country">{{ __('Abroad to Study') }}</label>
-            <select name="country">
+        <div class="my-4">
+            <div class="mb-1">
+                <label for="country">{{ __('Abroad to Study') }}</label>
+            </div>
+            <select name="country" class="rounded-lg">
                 <option value="USA 🇺🇸">USA</option>
                 <option value="UK 🇬🇧">UK</option>
                 <option value="Australia 🇦🇺">Australia</option>
@@ -57,21 +74,21 @@
             <x-input-error :messages="$errors->get('country')" class="mt-2" />
         </div> 
         
-        <div>
+        <div class="my-4">
             <label for="region">{{ __('Region') }}</label>
-            <input id="region" class="block mt-1 w-full" type="text" name="region" value="{{old('region')}}" required autofocus />
+            <input id="region" class="block mt-1 w-full rounded-lg" type="text" name="region" value="{{old('region')}}" required autofocus />
             <x-input-error :messages="$errors->get('region')" class="mt-2" />
         </div> 
         
-        <div>
+        <div class="my-4">
             <label for="start_date">{{ __('Start Date of Your Study Abroad') }}</label>
-            <input id="start_date" class="block mt-1 w-full" type="date" name="start_date" value="{{old('start_date')}}" required autofocus />
+            <input id="start_date" class="block mt-1 w-full rounded-lg" type="date" name="start_date" value="{{old('start_date')}}" required autofocus />
             <x-input-error :messages="$errors->get('start_date')" class="mt-2" />
         </div> 
         
-        <div>
+        <div class="my-4">
             <label for="end_date">{{ __('End Date of Your Study Abroad') }}</label>
-            <input id="end_date" class="block mt-1 w-full" type="date" name="end_date" value="{{old('end_date')}}" required autofocus />
+            <input id="end_date" class="block mt-1 w-full rounded-lg" type="date" name="end_date" value="{{old('end_date')}}" required autofocus />
             <x-input-error :messages="$errors->get('end_date')" class="mt-2" />
         </div> 
 
